d4d669758d8efe11fb267e8e4bb4ec92060e6393
AnkitKumar111 <ankitwelcomesyou@gmail.com>
2017-06-12 23:31:28 -0700

    Android_Assignment17.2
    A bound service to update the current time.

---
+++ app/src/main/java/com/example/ankitkumar/boundservice/BoundService.java
@@ -0,0 +1,40 @@
+package com.example.ankitkumar.boundservice;
+
+/**
+ * Created by Ankit Kumar on 6/12/2017.
+ */
+
+        import android.app.Service;
+        import android.content.Intent;
+        import android.os.Binder;
+        import android.os.IBinder;
+
+        import java.text.SimpleDateFormat;
+        import java.util.Date;
+        import java.util.Locale;
+
+
+public class BoundService extends Service {
+
+    //Base interface for a remotable object, the core part of a lightweight remote procedure call mechanism
+    private final IBinder myBinder = new MyLocalBinder();
+
+    //Binding Service
+    @Override
+    public IBinder onBind(Intent arg0) {
+        // TODO Auto-generated method stub
+        return myBinder;
+    }
+
+    //Formatting and parsing dates in a locale-sensitive manner
+    public String getCurrentTime() {
+        SimpleDateFormat dateformat = new SimpleDateFormat("HH:mm:ss  dd/MMM/yyyy", Locale.US);
+        return (dateformat.format(new Date()));
+    }
+
+    class MyLocalBinder extends Binder {
+        BoundService getService() {
+            return BoundService.this;
+        }
+    }
+}
